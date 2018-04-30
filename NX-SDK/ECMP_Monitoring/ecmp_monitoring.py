8426f26d1e3c9d9b4dc8a60a57e37c57a3038468
Nicolas Delecroix <ndelecro@cisco.com>
2018-04-30 15:31:24 +0200

    Use NX-SDK RibMgr APIs

--- NX-SDK/ECMP_Monitoring/ecmp_monitoring.py
+++ NX-SDK/ECMP_Monitoring/ecmp_monitoring.py
@@ -7,9 +7,6 @@ import threading
 import sys
 import os
 import json
-import re
-import urllib2
-from pprint import pprint
 # To gain access to Cisco NX-OS Infra SDK
 import nx_sdk_py
 
@@ -21,79 +18,44 @@ bw_threshold        = 10000000000
 intf_list           = []
 # In seconds
 DELAY_BETWEEN_CHECK = 5
+ribMgr              = 0
 
-def get_prefix_ifname(prefix):
-    global cliP
+def debug_log(str):
+    current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+    str_log = "%s -- %s" % (current_time, str)
+    file = open("/tmp/ecmp_monitoring.log", "a")
+    file.write(str_log + "\n")
+    file.close()
+    print(str_log)
 
-    print("get_prefix_ifname(%s)" % (str(prefix)))
-    show_cmd = "show ip route %s " % (str(prefix))
-    print(show_cmd)
-    resp_json = json.loads(cliP.execShowCmd(show_cmd, nx_sdk_py.R_JSON))
-    print(json.dumps(resp_json))
-    try:
-        row_prefix = resp_json["TABLE_vrf"]["ROW_vrf"]["TABLE_addrf"]["ROW_addrf"]["TABLE_prefix"]["ROW_prefix"]
-    except KeyError:
-        return ""
-    if (int(row_prefix["ucast-nhops"]) != 1):
-        print("ucast-nhops != 1, returning")
-        return ""    
-    try:
-        ifname = row_prefix["TABLE_path"]["ROW_path"]["ifname"]
-    except KeyError:
-        return ""
-    print(ifname)
-    return ifname
-
-def find_ecmp_intf():
-    global cliP, intf_list
-    found = False
-
-    show_cmd = "show ip route"
-    resp_json = json.loads(cliP.execShowCmd(show_cmd, nx_sdk_py.R_JSON))
-    resp_base = resp_json["TABLE_vrf"]["ROW_vrf"]["TABLE_addrf"]["ROW_addrf"]["TABLE_prefix"]["ROW_prefix"]
-    for prefix in resp_base:
-        print prefix["ipprefix"]
-        print prefix["ucast-nhops"]
-        if (int(prefix["ucast-nhops"]) > 1):
-            prefix_ecmp = prefix["ipprefix"]
-            for nexthop in prefix["TABLE_path"]["ROW_path"]:
-                intf = get_prefix_ifname(nexthop["ipnexthop"])
-                if (intf != ""):
-                    intf_list.append(intf)
-            found = True
-            break
-    print intf_list
-    if (found):
-        syslog_str = "[%s] Found an ECMP bundle: %s --> %s" % \
-                          (sdk.getAppName(), prefix_ecmp, ', '.join(map(str, intf_list)))
-        print(syslog_str)
-        t = sdk.getTracer()
-        t.event(str(syslog_str))
+def dump(obj):
+   for attr in dir(obj):
+       if hasattr( obj, attr ):
+           debug_log("obj.%s = %s" % (attr, getattr(obj, attr)))
 
 def check_intf_load(intf):
     global cliP, sdk
 
     t = sdk.getTracer()
-    print("check_intf_load()")
-
     show_cmd = "show interface %s" % (str(intf))
     resp_json = json.loads(cliP.execShowCmd(show_cmd, nx_sdk_py.R_JSON))
     # if there's no entries, exit
     if ("TABLE_interface" not in resp_json):
         return
-
     out_rate = resp_json["TABLE_interface"]["ROW_interface"]["eth_outrate1_bits"]
-    print out_rate
     if (int(out_rate) > bw_threshold):
         syslog_str = "[%s] High egress bandwidth: int=%s, bw=%s" % \
                       (sdk.getAppName(), intf, out_rate)
         t.syslog(t.WARNING, str(syslog_str))
+        debug_log(syslog_str)
 
 def get_telemetry_json():
     fields = ["interface", "eth_outrate1_bits"]
 
+    debug_log("get_telemetry_json()")
     for intf in intf_list:
         show_cmd = "show interface %s" % (str(intf))
+        debug_log(show_cmd)
         resp_json = json.loads(cliP.execShowCmd(show_cmd, nx_sdk_py.R_JSON))
         if ("TABLE_interface" not in resp_json):
             continue
@@ -108,39 +70,66 @@ def get_telemetry_json():
     return "{}"
 
 class pyCmdHandler(nx_sdk_py.NxCmdHandler):
-        def postCliCb(self,clicmd):
-                global cliP, pattern_path_dict, bw_threshold
+    def postCliCb(self,clicmd):
+        global cliP, pattern_path_dict, bw_threshold
 
-                if clicmd.isKeywordSet("tm-use-only"):
-                    print "tm-use"
-                    tel_json = get_telemetry_json()
-                    clicmd.printConsole(json.dumps(tel_json))
+        if (clicmd.isKeywordSet("tm-use-only")):
+            debug_log("tm-use")
+            tel_json = get_telemetry_json()
+            debug_log(json.dumps(tel_json))
+            clicmd.printConsole(json.dumps(tel_json))
+            return True
+        if ("set_bw_threshold_cmd" in clicmd.getCmdName()):
+            if ("no" in clicmd.getCmdLineStr()):
+                debug_log("no set_bw_threshold_cmd")
+                bw_threshold = 10000000000
+            else:
+                debug_log("set_bw_threshold_cmd")
+                # Create Int Pointer to get the integer Value
+                int_p = nx_sdk_py.new_intp()
+                int_p = nx_sdk_py.void_to_int(clicmd.getParamValue("<bw-threshold-value>"))
+                # Get the value of int * in python
+                if int_p:
+                    bw_threshold = int(nx_sdk_py.intp_value(int_p)) * 1000000
+                debug_log(bw_threshold)
+        return True
 
-                if "set_bw_threshold_cmd" in clicmd.getCmdName():
-                   if "no" in clicmd.getCmdLineStr():
-                       print "delete cmd"
-                       bw_threshold = 10000000000
-                   else:
-                       print "add cmd"
-                       # Create Int Pointer to get the integer Value
-                       int_p = nx_sdk_py.new_intp()
-                       int_p = nx_sdk_py.void_to_int(clicmd.getParamValue("<bw-threshold-value>"))
-                       # Get the value of int * in python
-                       if int_p:
-                           bw_threshold = int(nx_sdk_py.intp_value(int_p)) * 1000000
-                       print bw_threshold
-                return True
+### Overloaded NxRibMgrHandler class.
+class pyRibHandler(nx_sdk_py.NxRibMgrHandler):
+    def postL3RouteCb(self,nxroute):
+        global intf_list, ribMgr
+
+        debug_log("postL3RouteCb")
+        prefix   = nxroute.getAddress() + str("/") + str(nxroute.getMaskLen())
+        debug_log(prefix)
+
+        if (nxroute.getL3NextHopCount() > 1):
+            debug_log("%s is a ECMP route" % prefix)
+            # It's a ECMP route, let's get the outgoing interfaces
+            nextHop = nxroute.getL3NextHop(True)
+            while nextHop:
+                debug_log("----> while nextHop")
+                outAddr = nextHop.getAddress()
+                outInterface = nextHop.getOutInterface()
+                debug_log("----> %s, %s" % (outAddr, outInterface))
+                # Let's get the actual p2p link outgoing interface
+                p2pIntfRoute = ribMgr.getL3Route(outAddr)
+                p2pIntfNextHop = p2pIntfRoute.getL3NextHop()
+                p2pIntfName = p2pIntfNextHop.getOutInterface()
+                debug_log("----> p2pIntfName: %s" % p2pIntfName)
+                intf_list.append(p2pIntfName)
+                nextHop = nxroute.getL3NextHop(False)
+
+        debug_log("end postL3RouteCb")
+        return True
 
 def timerThread(name,val):
     global cliP, sdk
 
     while True:
-        if sdk and cliP:
-           print "timer kicked - sdk"
-           for intf in intf_list:
-               check_intf_load(intf)
-        else:
-           print "timer ticked - not sdk"
+        if (sdk and cliP):
+            for intf in intf_list:
+                check_intf_load(intf)
         time.sleep(DELAY_BETWEEN_CHECK)
 
 # Perform the event handling loop in a dedicated Python thread.
@@ -148,13 +137,13 @@ def timerThread(name,val):
 # may continue to do other work.  The call to startEventLoop will
 # block until we break out of it by calling stopEventLoop on
 def evtThread(name,val):
-    global cliP, sdk, event_hdlr
+    global cliP, sdk, event_hdlr, ribMgr
 
     sdk = nx_sdk_py.NxSdk.getSdkInst(len(sys.argv), sys.argv)
-    if not sdk:
+    if (not sdk):
         return
 
-    sdk.setAppDesc('ECMP Monitoring')
+    sdk.setAppDesc("ECMP Monitoring")
 
     t = sdk.getTracer()
     t.event("[%s] Started service" % sdk.getAppName())
@@ -176,20 +165,22 @@ def evtThread(name,val):
     cliP.setCmdHandler(mycmd)
 
     # Install the new commands to the NXOS parse tree
-    # t.event("Adding commands to CLIS parse tree")
     cliP.addToParseTree()
 
-    find_ecmp_intf()
+    ribMgr = sdk.getRibMgr()
+    myribcb = pyRibHandler()
+    ribMgr.setRibMgrHandler(myribcb)
+    ribMgr.watchL3Route("bgp", "65000")
 
-    # Block in the event loop to service NXOS messages
-    print("Starting event loop, CTRL-C to interrupt")
+    # Block in the event loop to service NX-OS messages
+    debug_log("Starting event loop, CTRL-C to interrupt")
     sdk.startEventLoop()
 
     # Got here by calling stopEventLoop from signal handler
     t.event("Service Quitting...!")
     event_hdlr = False
 
-    # [Required] Needed for graceful exit.
+    # Needed for graceful exit.
     nx_sdk_py.NxSdk.__swig_destroy__(sdk)
 
 # Create a timer thread
@@ -204,5 +195,3 @@ evt_thread = threading.Thread(target=evtThread, args=("evtThread",0))
 evt_thread.start()
 timer_thread.start()
 evt_thread.join()
-
-
