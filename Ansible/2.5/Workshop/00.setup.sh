f3216c9b459785f4c9c3625804567ce0cfa84f25
Nicolas Delecroix <ndelecro@cisco.com>
2019-01-21 11:19:52 +0100

    Ansible workshop

---
+++ Ansible/2.5/Workshop/00.setup.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 student_id"
+    exit 1
+fi
+
+id=$1
+
+for file in *.{yml,txt}; do
+    sed -i '' "s/STUDENT_ID/$id/" $file
+    echo $file
+done
