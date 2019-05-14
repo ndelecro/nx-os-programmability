1c12fedb6858b85667b73c0f050369899dcdbdc8
Nicolas Delecroix <ndelecro@cisco.com>
2019-05-14 09:27:42 +0200

    Ansible lab

--- Ansible/Workshop/00.setup.sh
+++ Ansible/Workshop/00.setup.sh
@@ -7,10 +7,18 @@ fi
 
 id=$1
 
+re='^[0-9]+$'
+if ! [[ $id =~ $re ]] ; then
+    echo "Error: student_id must be a number" >&2
+    exit 1
+fi
+
 for file in *.{yml,txt,j2}; do
     sed -i "s/STUDENT_ID/$id/" $file
     echo $file
 done
 
-sed -i "s/STUDENT_ID/$id/" /etc/ansible/host_vars/n9kv-{1,2}
 sed -i "s/STUDENT_ID/$id/" /etc/ansible/group_vars/nxos_vteps
+
+echo 172.16.255.50 n9kv-1 >> /etc/hosts
+echo 172.16.255.51 n9kv-2 >> /etc/hosts
