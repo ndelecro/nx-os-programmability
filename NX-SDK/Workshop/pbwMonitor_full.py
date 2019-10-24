1ab2b0540d5f69a969cd3876d9d351acc8f03efa
Nicolas Delecroix <ndelecro@cisco.com>
2019-10-24 10:39:44 -0700

    NX-SDK workshop

--- NX-SDK/Workshop/pbwMonitor_full.py
+++ NX-SDK/Workshop/pbwMonitor_full.py
@@ -20,9 +20,25 @@ sdk                             = 0
 event_hdlr                      = True
 mycmd                           = 0
 port_threshold                  = 100
+sdk_shutdown                    = False
+sdk_lock                        = threading.Lock()
+
+def check_sdk_valid():
+    global sdk, sdk_shutdown
+
+    if not sdk:
+       return False
+
+    if sdk_shutdown:
+       return False
+    return True
+
 
 def print_port_bw(result, print_syslog, hit_threshold):
     global sdk, port_threshold
+
+    if not check_sdk_valid():
+       return
 
     t = sdk.getTracer()
     (interface, eth_bw, tx_rate, rx_rate, tx_bits, rx_bits) = ("", 0, 0, 0, 0, 0)
@@ -50,13 +66,13 @@ def print_port_bw(result, print_syslog, hit_threshold):
     tx_percentage = float(-1) # N/A
     rx_percentage = float(-1) # N/A
 
-    if eth_bw: 
+    if eth_bw:
        # For Test purposes
-       tx_bits = random.randint(eth_bw, eth_bw * 1000) 
+       tx_bits = random.randint(eth_bw, eth_bw * 1000)
 
-       tx_percentage = float((tx_bits * 100)/(eth_bw * 1000)) 
+       tx_percentage = float((tx_bits * 100)/(eth_bw * 1000))
        rx_percentage = float((rx_bits * 100)/(eth_bw * 1000))
-      
+
     display_op = False
     if not hit_threshold:
        display_op = True
@@ -66,14 +82,14 @@ def print_port_bw(result, print_syslog, hit_threshold):
        if hit_threshold:
           display_op = True
        if t and print_syslog:
-          t.event("Port: %s (tx: %.2f, rx: %.2f) higher than threshold %d" % \
+          t.syslog(nx_sdk_py.NxTrace.ALERT, "Port: %s (tx: %.2f, rx: %.2f) higher than threshold %d" % \
             (interface, tx_percentage, rx_percentage, port_threshold))
-       
+
     print_str = ""
-    if display_op:        
+    if display_op:
         # Return the print string for show output.
         print_str = '{0:15} {1:20} {2:18.2f} {3:18.2f}'.format(interface, \
-            bw_str, tx_percentage, rx_percentage)      
+            bw_str, tx_percentage, rx_percentage)
     return print_str
 
 class pyCmdHandler(nx_sdk_py.NxCmdHandler):
@@ -81,7 +97,7 @@ class pyCmdHandler(nx_sdk_py.NxCmdHandler):
         global cliP, check_interval, port_threshold
 
         if "show_port_bw_util_cmd" in clicmd.getCmdName():
-           
+
              # Get the port value if set
              port = nx_sdk_py.void_to_string(clicmd.getParamValue("<port>"))
              if port:
@@ -96,7 +112,7 @@ class pyCmdHandler(nx_sdk_py.NxCmdHandler):
                 "Bw (Rx/Tx Sec Rate)", "Tx_BW percentage", "RX_BW percentage")
              clicmd.printConsole("%s\n" % print_str)
              clicmd.printConsole("--------------- -------------------- " + \
-                "------------------ ------------------\n") 
+                "------------------ ------------------\n")
 
              # Nothing to display
              if not result:
@@ -105,7 +121,7 @@ class pyCmdHandler(nx_sdk_py.NxCmdHandler):
              # Parse the JSON output
              json_res = json.loads(result)["TABLE_interface"]["ROW_interface"]
              for key in json_res:
-                 
+
                  # Handle nested JSON output for all ports
                  if type(key) == dict:
                     display_op = print_port_bw(key, False, clicmd.isKeywordSet("hit-threshold"))
@@ -121,7 +137,7 @@ class pyCmdHandler(nx_sdk_py.NxCmdHandler):
         elif "port_bw_threshold_cmd" in clicmd.getCmdName():
              # Action for port_bw_threshold_cmd
 
-             # Handle no command. Reset to default 
+             # Handle no command. Reset to default
              if "no" in clicmd.getCmdLineStr():
                 port_threshold = 100;
              else:
@@ -148,7 +164,7 @@ def setup_config_cmd():
 
     cliP = sdk.getCliParser()
     # Note: since we have already updated the keyword information for
-    # "port" and "bw" earlier, we don't have to update for each and 
+    # "port" and "bw" earlier, we don't have to update for each and
     # every cmd as its information will be automatically picked up for
     # other commands.
     nxcmd = cliP.newConfigCmd("port_bw_threshold_cmd", "port bw threshold <threshold>")
@@ -177,20 +193,38 @@ def setup_clis():
     cliP.setCmdHandler(mycmd)
 
     # Add custom commands to NX CLI Parse Tree
-    cliP.addToParseTree()    
+    cliP.addToParseTree()
 
 def timerThread(name, val):
-    global cliP, sdk
+    global cliP, sdk, sdk_shutdown, sdk_lock
 
-    while True:
+    while check_sdk_valid():
+        if not cliP:
+           time.sleep(1)
+           continue
+
         # Get show int output in JSON format and get the required fields
+
+        # Lock to syncronize with other threads
+        sdk_lock.acquire()
+        if not check_sdk_valid():
+           sdk_lock.release()
+           return
         result = cliP.execShowCmd("show int", nx_sdk_py.R_JSON)
+        sdk_lock.release()
+
         if result:
            json_res = json.loads(result)["TABLE_interface"]["ROW_interface"]
            for key in json_res:
                if type(key) == dict:
+                    sdk_lock.acquire()
+                    if not check_sdk_valid():
+                       sdk_lock.release()
+                       return
                     # FIXME4: remove the pass statement, and uncomment print_port_bw
+                    # pass
                     print_port_bw(key, True, False)
+                    sdk_lock.release()
         time.sleep(10)
 
 # Perform the event handling loop in a dedicated Python thread.
@@ -198,17 +232,23 @@ def timerThread(name, val):
 # may continue to do other work.  The call to startEventLoop will
 # block until we break out of it by calling stopEventLoop on
 def evtThread(name,val):
-    global sdk, event_hdlr
+    global sdk, event_hdl, sdk_shutdown, sdk_lock
 
     sdk = nx_sdk_py.NxSdk.getSdkInst(len(sys.argv), sys.argv)
     if not sdk:
-        return
+       sdk_shutdown = True
+       return
 
     # FIXME1: uncomment the following lines
     t = sdk.getTracer()
-    t.event("[%s] Started app" % sdk.getAppName())
+    t.syslog(nx_sdk_py.NxTrace.ALERT, "[%s] Started app" % sdk.getAppName())
 
     setup_clis()
+
+    # Create a timer thread
+    timer_thread = threading.Thread(target=timerThread, args=("timerThread",0))
+    timer_thread.daemon = True
+    timer_thread.start()
 
     # Block in the event loop to service NXOS messages
     print("Starting event loop, CTRL-C to interrupt")
@@ -217,18 +257,39 @@ def evtThread(name,val):
     # Got here by calling stopEventLoop from signal handler
     event_hdlr = False
 
+    sdk_lock.acquire()
     # [Required] Needed for graceful exit.
     nx_sdk_py.NxSdk.__swig_destroy__(sdk)
+    sdk_shutdown = True
+    sdk = 0
+    cliP = 0
+    sdk_lock.release()
 
-# Create a timer thread
-timer_thread = threading.Thread(target=timerThread, args=("timerThread",0))
-timer_thread.daemon = True
+def exit_gracefully(signum, frame):
+    global sdk
+    try:
+       if sdk:
+          while not sdk.isInEventLoop():
+              time.sleep(1)
+          sdk.stopEventLoop()
+    except KeyboardInterrupt:
+       print("catch sigterm in except")
+
+def set_signals():
+    signal.signal(signal.SIGINT, exit_gracefully)
+    signal.signal(signal.SIGTERM, exit_gracefully)
+
+set_signals()
 
 # Create an event thread to register with NxSDK, CLI Parser, Tracer
 # and Start Event loop to keep the application running.
 # NOTE: Doesnt need a special Event Thread can be done on Main too.
 evt_thread = threading.Thread(target=evtThread, args=("evtThread",0))
-
+evt_thread.daemon = True
 evt_thread.start()
-timer_thread.start()
-evt_thread.join()
+
+# To catch ctrl-c & kill
+while not sdk_shutdown:
+    time.sleep(1)
+
+sys.exit(0)
